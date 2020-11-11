0ea9184dae8aaa5e85eb60755d1b230628dec49f
robertb-cubeia <sodwhitefinger@gmail.com>
2020-11-11 01:44:20 +0100

    [TASK-10] - Add mapper test

--- src/test/java/com/bialas/robert/devicemanager/mapper/DeviceDTOMapperTest.java
+++ src/test/java/com/bialas/robert/devicemanager/mapper/DeviceDTOMapperTest.java
@@ -1,10 +1,78 @@
 package com.bialas.robert.devicemanager.mapper;
 
-import org.junit.Ignore;
+import com.bialas.robert.devicemanager.domain.dto.DeviceDTO;
+import com.bialas.robert.devicemanager.domain.entity.Device;
+import com.bialas.robert.devicemanager.util.DeviceTestUtils;
+import org.junit.Test;
 
-@Ignore
+import java.time.Clock;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+import static junit.framework.TestCase.assertEquals;
+
 public class DeviceDTOMapperTest {
 
-    //todo - include mapper test (due to lack of time not implemented and yet it would be very useful to check if the mapper behaves as expected), so just a placeholder now
+    private DeviceDTOMapper deviceDTOMapper = new DeviceDTOMapperImpl();
+
+    @Test
+    public void givenDevice_whenToDeviceDtoCalled_returnDeviceDto() {
+        //given
+        Device device = DeviceTestUtils.getExpectedDeviceAfterCreation();
+
+        //when
+        DeviceDTO result = deviceDTOMapper.toDeviceDTO(device);
+
+        //then
+        assertEquals(DeviceTestUtils.getExpectedDeviceDTO(ZonedDateTime.now(Clock.fixed(Instant.ofEpochSecond(1000), ZoneId.of("UTC")))),result);
+    }
+
+    @Test
+    public void givenNullDevice_whenToDeviceDtoCalled_returnNull() {
+        //when
+        DeviceDTO result = deviceDTOMapper.toDeviceDTO(null);
+
+        //then
+        assertEquals(null, result);
+    }
+
+    @Test
+    public void givenDeviceDTO_whenUpdateDeviceFromDTOCalled_thenUpdateDevice() {
+        //given
+        Device device = DeviceTestUtils.getExpectedDeviceAfterCreation();
+        Device expectedDevice = Device.builder()
+                .id(device.getId())
+                .deviceName("newName")
+                .brand(device.getBrand())
+                .creationTime(device.getCreationTime())
+                .build();
+        DeviceDTO deviceDTOToUpdateFrom = DeviceDTO.builder()
+                .deviceName("newName")
+                .build();
+
+        //when
+        deviceDTOMapper.updateDeviceFromDTO(deviceDTOToUpdateFrom, device);
+
+        //then
+        assertEquals(expectedDevice, device);
+    }
+
+    @Test
+    public void givenNullDTO_whenUpdateDeviceFromDTOCalled_thenDoNothingToDevice() {
+        Device device = DeviceTestUtils.getExpectedDeviceAfterCreation();
+        Device expectedDevice = Device.builder()
+                .id(device.getId())
+                .deviceName(device.getDeviceName())
+                .brand(device.getBrand())
+                .creationTime(device.getCreationTime())
+                .build();
+
+        //when
+        deviceDTOMapper.updateDeviceFromDTO(null, device);
+
+        //then
+        assertEquals(expectedDevice, device);
+    }
 
 }
